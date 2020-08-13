444923be268abe80b31673bd1ba3db4495108dbb
Julian Lawrance <julian.lawrance@gmail.com>
2020-08-13 19:15:34 +0100

    Add script for drawing a tower's range when the mouse is over it

---
+++ Assets/Scripts/Util/MethodLogger.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+namespace Assets.Scripts.Util
+{
+    public class MethodLogger : IDisposable
+    {
+        /// <summary>
+        /// The class name.
+        /// </summary>
+        private readonly string ClassName;
+
+        /// <summary>
+        /// The method name.
+        /// </summary>
+        private readonly string MethodName;
+
+        public MethodLogger(
+            string callerClassName,
+            [CallerMemberName] string callerMethodName = "")
+        {
+            ClassName = callerClassName;
+            MethodName = callerMethodName;
+        }
+
+        public void Log(string message)
+        {
+            Debug.Log($"{ClassName}.{MethodName}(): {message}");
+        }
+
+        public void LogWarning(string message)
+        {
+            Debug.LogWarning($"{ClassName}.{MethodName}(): {message}");
+        }
+
+        public void LogError(string message)
+        {
+            Debug.LogError($"{ClassName}.{MethodName}(): {message}");
+        }
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+    }
+}
