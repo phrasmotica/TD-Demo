06415a1f7ac130db44a2526b892efbb25c79126f
Julian Lawrance <julian.lawrance@gmail.com>
2020-08-13 19:06:26 +0100

    Create scripts for enemy data and movement

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Enemy : MonoBehaviour
+    {
+        /// <summary>
+        /// The enemy's starting health.
+        /// </summary>
+        [Range(1, 5)]
+        public int StartingHealth;
+
+        /// <summary>
+        /// The money reward for killing this enemy.
+        /// </summary>
+        [Range(1, 10)]
+        public int Reward;
+
+        /// <summary>
+        /// The enemy's current health.
+        /// </summary>
+        private int health;
+
+        /// <summary>
+        /// Delegate to run on death.
+        /// </summary>
+        public Action<int> OnKill { private get; set; }
+
+        /// <summary>
+        /// Set the enemy's health.
+        /// </summary>
+        private void Start()
+        {
+            health = StartingHealth;
+        }
+
+        /// <summary>
+        /// Destroy this object if it's dead.
+        /// </summary>
+        private void Update()
+        {
+            if (health <= 0)
+            {
+                Destroy(gameObject);
+                OnKill(Reward);
+            }
+        }
+
+        /// <summary>
+        /// If hit by a projectile, take damage and destroy the projectile.
+        /// </summary>
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            var otherObj = collision.gameObject;
+
+            var projectileComponent = otherObj.GetComponent<Projectile>();
+            if (projectileComponent != null)
+            {
+                health -= projectileComponent.Damage;
+                Destroy(otherObj);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class EnemyMovement : MonoBehaviour
+    {
+        /// <summary>
+        /// The enemy's initial velocity.
+        /// </summary>
+        [Range(0, 10)]
+        public int InitialVelocity;
+
+        /// <summary>
+        /// Give the enemy some velocity.
+        /// </summary>
+        void Start()
+        {
+            var rb = gameObject.GetComponent<Rigidbody2D>();
+            rb.velocity = new Vector2(InitialVelocity, 0);
+        }
+    }
+}
