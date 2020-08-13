99c1fcbad47e4e63a9b4c7101a4fae9e6c16eff6
Julian Lawrance <julian.lawrance@gmail.com>
2020-08-13 23:03:09 +0100

    Add script for creating towers. Attach it to controller object

---
+++ Assets/Scripts/Controller/CreateTowers.cs
@@ -0,0 +1,25 @@
+﻿using Assets.Scripts.Util;
+using UnityEngine;
+
+namespace Assets.Scripts.Controller
+{
+    public class CreateTowers : MonoBehaviour
+    {
+        /// <summary>
+        /// The tower prefab.
+        /// </summary>
+        public GameObject TowerPrefab;
+
+        /// <summary>
+        /// Creates a tower.
+        /// </summary>
+        public void CreateTower()
+        {
+            using (var logger = new MethodLogger(nameof(CreateTowers)))
+            {
+                logger.Log("Creating tower");
+                Instantiate(TowerPrefab);
+            }
+        }
+    }
+}
