da3c338cd2631d0cb2f69aaca5af3d794ceca6a5
Julian Lawrance <julian.lawrance@gmail.com>
2020-08-13 22:44:30 +0100

    Change colliders to triggers

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -50,7 +50,7 @@ namespace Assets.Scripts
         /// <summary>
         /// If hit by a projectile, take damage and destroy the projectile.
         /// </summary>
-        private void OnCollisionEnter2D(Collision2D collision)
+        private void OnTriggerEnter2D(Collider2D collision)
         {
             var otherObj = collision.gameObject;
 
