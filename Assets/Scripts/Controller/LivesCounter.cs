e952a1d69fd858b061219586ba56339f2f62928b
Julian Lawrance <julian.lawrance@gmail.com>
2020-08-14 20:58:50 +0100

    Create lives counter script and add end zone object for deducting lives when enemies reach it

---
+++ Assets/Scripts/Controller/LivesCounter.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Controller
+{
+    public class LivesCounter : MonoBehaviour
+    {
+        /// <summary>
+        /// The number of starting lives.
+        /// </summary>
+        [Range(1, 50)]
+        public int StartingLives;
+
+        /// <summary>
+        /// The number of lives.
+        /// </summary>
+        private int Lives;
+
+        /// <summary>
+        /// Start is called before the first frame update.
+        /// </summary>
+        private void Start()
+        {
+            Lives = StartingLives;
+        }
+
+        /// <summary>
+        /// Adds the given amount of lives to the current count.
+        /// </summary>
+        public void AddLives(int amount)
+        {
+            Lives += amount;
+        }
+
+        /// <summary>
+        /// Renders a GUI.
+        /// </summary>
+        public void OnGUI()
+        {
+            GUI.Label(new Rect(0, 60, 160, 30), $"Lives {Lives}");
+        }
+    }
+}
---
+++ Assets/Scripts/Path/EndZone.cs
@@ -0,0 +1,28 @@
+﻿using Assets.Scripts.Controller;
+using UnityEngine;
+
+namespace Assets.Scripts.Path
+{
+    public class EndZone : MonoBehaviour
+    {
+        /// <summary>
+        /// The lives counter.
+        /// </summary>
+        public LivesCounter LivesCounter;
+
+        /// <summary>
+        /// When an enemy collides, deduct a life and destroy the enemy.
+        /// </summary>
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            var otherObj = collision.gameObject;
+
+            var enemyComponent = otherObj.GetComponent<Enemy>();
+            if (enemyComponent != null)
+            {
+                LivesCounter.AddLives(-1);
+                Destroy(otherObj);
+            }
+        }
+    }
+}
