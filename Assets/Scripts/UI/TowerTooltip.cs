45912c8b70af3c30ea2e88c7a13972287f4c7b48
Julian Lawrance <julian.lawrance@gmail.com>
2022-04-01 12:47:25 +0100

    Create prefab for a tooltip to show when hovering over a button for creating a tower

---
+++ Assets/Scripts/UI/TowerTooltip.cs
@@ -0,0 +1,13 @@
+﻿using TDDemo.Assets.Scripts.Extensions;
+using UnityEngine;
+
+namespace TDDemo.Assets.Scripts.UI
+{
+    public class TowerTooltip : MonoBehaviour
+    {
+        private void Update()
+        {
+            transform.FollowMouse();
+        }
+    }
+}
---
+++ Assets/Scripts/UI/TowerTooltipOnHover.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace TDDemo.Assets.Scripts.UI
+{
+    public class TowerTooltipOnHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    {
+        public Canvas UiCanvas;
+
+        public GameObject TooltipPrefab;
+
+        private GameObject _currentTooltip;
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            _currentTooltip = Instantiate(TooltipPrefab, UiCanvas.transform);
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            Destroy(_currentTooltip);
+            _currentTooltip = null;
+        }
+    }
+}
