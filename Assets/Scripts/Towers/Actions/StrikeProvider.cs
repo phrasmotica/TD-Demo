e8971b40ea9bd6c9fd4952b04530f2fbdef409eb
Julian Lawrance <julian.lawrance@gmail.com>
2022-04-02 22:57:08 +0100

    Create abstract StrikeProvider class

---
+++ Assets/Scripts/Towers/Actions/StrikeProvider.cs
@@ -0,0 +1,10 @@
+﻿using TDDemo.Assets.Scripts.Towers.Strikes;
+using UnityEngine;
+
+namespace TDDemo.Assets.Scripts.Towers.Actions
+{
+    public abstract class StrikeProvider : MonoBehaviour
+    {
+        public abstract IStrike CreateStrike();
+    }
+}
