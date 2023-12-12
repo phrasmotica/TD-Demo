5ba38e9d119bfa03ce4b5fb561e73f02909e9f76
Julian L <julian.lawrance@gmail.com>
2023-12-12 13:30:28 +0000

    Create script for a distraction source

---
+++ Assets/Scripts/Distractions/DistractionSource.cs
@@ -0,0 +1,11 @@
+using TDDemo.Assets.Scripts.Util;
+using UnityEngine;
+
+namespace TDDemo.Assets.Scripts.Distractions
+{
+    public class DistractionSource : BaseBehaviour
+    {
+        [Range(30, 60f)]
+        public int Price;
+    }
+}
