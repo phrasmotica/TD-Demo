f6af49c7c9e7ab766237d1128900148425ff62bd
Julian L <julian.lawrance@gmail.com>
2023-12-12 13:36:40 +0000

    Add button for creating a dropped coin distraction

---
+++ Assets/Scripts/UI/CreateDistraction.cs
@@ -0,0 +1,26 @@
+﻿using TDDemo.Assets.Scripts.Controller;
+using TDDemo.Assets.Scripts.Distractions;
+using TDDemo.Assets.Scripts.Util;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace TDDemo.Assets.Scripts.UI
+{
+    public class CreateDistraction : MonoBehaviour
+    {
+        public DistractionSource DistractionSource;
+
+        public BankManager Bank;
+
+        public Button Button;
+
+        public Image Image;
+
+        public void SetInteractable()
+        {
+            var canAfford = Bank.CanAfford(DistractionSource.Price) != PurchaseMethod.None;
+            Button.interactable = canAfford;
+            Image.color = canAfford ? ColourHelper.FullOpacity : ColourHelper.HalfOpacity;
+        }
+    }
+}
