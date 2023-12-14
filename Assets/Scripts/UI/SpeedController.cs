46395d8478a26c67ab8f7882abe8e3268bb60524
Julian L <julian.lawrance@gmail.com>
2023-12-14 21:27:42 +0000

    Allow pausing time by pressing spacebar

--- Assets/Scripts/UI/SpeedController.cs
+++ Assets/Scripts/UI/SpeedController.cs
@@ -5,26 +5,63 @@ namespace TDDemo.Assets.Scripts.UI
 {
     public class SpeedController : MonoBehaviour
     {
-        private const int FastForwardSpeed = 3;
+        private const float FastForwardSpeed = 3;
 
-        private bool _isOn;
+        private bool _isPaused;
+
+        private bool _isFastForward;
+
+        private float _lastSpeed;
+
+        public UnityEvent OnPause;
+
+        public UnityEvent OnResume;
 
         public UnityEvent OnFastForward;
 
         public UnityEvent OnNormalSpeed;
 
+        private void Update()
+        {
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                TogglePause();
+            }
+        }
+
+        public void TogglePause()
+        {
+            _isPaused = !_isPaused;
+
+            if (_isPaused)
+            {
+                Debug.Log("Paused");
+                _lastSpeed = Time.timeScale;
+                Time.timeScale = 0;
+                OnPause.Invoke();
+            }
+            else
+            {
+                Debug.Log("Resumed");
+                Time.timeScale = _lastSpeed;
+                OnResume.Invoke();
+            }
+        }
+
         public void ToggleFastForward()
         {
-            _isOn = !_isOn;
+            _isFastForward = !_isFastForward;
 
-            Time.timeScale = _isOn ? FastForwardSpeed : 1;
-
-            if (_isOn)
+            if (_isFastForward)
             {
+                Debug.Log($"Fast forward x{FastForwardSpeed}");
+                Time.timeScale = FastForwardSpeed;
                 OnFastForward.Invoke();
             }
             else
             {
+                Debug.Log("Normal speed");
+                Time.timeScale = 1;
                 OnNormalSpeed.Invoke();
             }
         }
